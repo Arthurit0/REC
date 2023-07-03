a8aa26ce1f83a2241110bf12692612234264156c
Arthur José <arthur.jose27@gmail.com>
2023-07-03 15:42:14 -0300

    Update scriptServer.py
    Agora pega o primeiro IP local da interface

--- Trabalho_Final/scriptServer.py
+++ Trabalho_Final/scriptServer.py
@@ -6,7 +6,15 @@ import os
 
 
 def my_ip():
-    return socket.gethostbyname(socket.gethostname())
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("10.255.255.255", 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = "127.0.0.1"
+    finally:
+        s.close()
+    return IP
 
 
 def start_iperf_server(port, num, json):
@@ -58,7 +66,7 @@ def main():
 
     print()
 
-    old_server_files = glob.glob('server*.json')
+    old_server_files = glob.glob("server*.json")
     for file in old_server_files:
         os.remove(file)
 
